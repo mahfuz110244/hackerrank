7f35515083f4863bfcdb877c1cf11017f84424bf
mahfuz <mahfuz@misfit.tech>
2020-06-01 06:53:10 +0600

    [swap-case-2] add swap-case challenge

---
+++ swap_case.py
@@ -0,0 +1,23 @@
+#  2. swap-list : https://www.hackerrank.com/challenges/swap-case
+def swap_case1(input_string):
+    input_string = list(input_string)
+    toggle_string = ''
+    for value in input_string:
+        ascii_value = ord(value)
+        if 65 <= ascii_value <= 90:
+            toggle_string += chr(ascii_value + 32)
+        elif 97 <= ascii_value <= 122:
+            toggle_string += chr(ascii_value - 32)
+        else:
+            toggle_string += value
+    return toggle_string
+
+
+def swap_case(input_string):
+    return ''.join(value.lower() if value.isupper() else value.upper() for value in input_string)
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
