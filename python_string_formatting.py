c3a56729b8439c7cb622843e7b8ea9f18a44263d
mahfuz <mahfuz@misfit.tech>
2020-06-07 10:11:01 +0600

    [python-string-formatter] solve the problem

---
+++ python_string_formatting.py
@@ -0,0 +1,12 @@
+def print_formatted(number):
+    # your code goes here
+    # For spacing between the each column, based on the integer limit given.
+    w = len(bin(number)[2:])
+    for i in range(1, number+1):
+        print(str(i).rjust(w, ' '), str(oct(i)[2:]).rjust(w, ' '), str(hex(i)[2:].upper()).rjust(w, ' '),
+              str(bin(i)[2:]).rjust(w, ' '))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
