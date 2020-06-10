57477e1a76a3e1dbce462597624c98877a52b8b5
mahfuz <mahfuz@misfit.tech>
2020-06-11 01:14:54 +0600

    [merge-the-tools] add another way for loop

--- merge_the_tools.py
+++ merge_the_tools.py
@@ -3,11 +3,17 @@ def merge_the_tools(string, k):
     # your code goes here
     for i in range(0, len(string), k):
         sub_string = string[i:i+k:]
+
+        #  way 1
         sub_string_list = []
         for i in range(len(sub_string)):
             if sub_string[i] not in sub_string_list:
                 sub_string_list.append(sub_string[i])
         print(''.join(sub_string_list))
+
+        # way 2
+        d = dict()
+        print(''.join([d.setdefault(c, c) for c in sub_string if c not in d]))
 
 
 if __name__ == '__main__':
