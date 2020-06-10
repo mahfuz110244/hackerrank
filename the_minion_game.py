dc7cb5d8bbb29bff71813daae7c43ed7e03014b3
mahfuz <mahfuz@misfit.tech>
2020-06-10 21:48:24 +0600

    [the-minion-game] solve problem using best technique

--- the_minion_game.py
+++ the_minion_game.py
@@ -22,15 +22,31 @@ def minion_game(string):
     # starting letter, you can add all substrings of different length that start with this letter. It will be len(s)
     # - i
 
+    # solution 1:
     vowels = 'AEIOU'
-
     kevsc = 0
     stusc = 0
+    strl = len(s)
     for i in range(len(s)):
         if s[i] in vowels:
-            kevsc += (len(s) - i)
+            kevsc += (strl - i)
         else:
-            stusc += (len(s) - i)
+            stusc += (strl - i)
+
+    if kevsc > stusc:
+        print("Kevin", kevsc)
+    elif kevsc < stusc:
+        print("Stuart", stusc)
+    else:
+        print("Draw")
+
+    # solution 2:
+    vowels = 'AEIOU'
+    strl = len(string)
+    kevsc = sum(strl - i for i in range(strl) if string[i] in vowels)
+    # 1+2+3+....+n=(n*(n+1))/2 That will be the number of all contiguous substrings and then you just need to
+    # substract those that Kevin got
+    stusc = int(strl * (strl + 1) / 2 - kevsc)
 
     if kevsc > stusc:
         print("Kevin", kevsc)
